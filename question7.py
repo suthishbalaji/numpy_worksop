62a9c559832312bb6020c4156bc25e20851b2e81
suthishbalaji <155551444+suthishbalaji@users.noreply.github.com>
2024-05-22 10:55:39 +0530

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,12 +1,16 @@
 # create a list as below
-output=[[1,2,3],[4,5,6],[7,5,3,2,3,[2,[3]]]]
-a=[1,2,3]
-b=[4,5,6]
-c=[7,5,3,2,3]
-d=[2]
-e=[3]
 
-# use these lists to create another list which will look like the list output in line 2
-
-# use append method to do this.
-
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 5, 3, 2, 3]
+d = [2]
+e = [3]
+output = []
+output.append(a) 
+output.append(b)  
+nested_list = c.copy()
+deep_nested_list = d.copy() 
+deep_nested_list.append(e)   
+nested_list.append(deep_nested_list) 
+output.append(nested_list)
+print(output)
